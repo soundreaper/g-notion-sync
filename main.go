d3e8fdd0f7235b5623cb88c29f2faa7bcb1b6a29
Subal Pant <email@example.com>
2021-10-03 20:43:24 -0700

    add proper comments to main

--- main.go
+++ main.go
@@ -11,56 +11,57 @@ import (
 	no "github.com/soundreaper/g-notion-sync/pkg/notion"
 )
 
-
-
-
 func main() {
 	ctx := context.Background()
 	// Configuration
 
-	// Notion
-	notionKey := config.GetNotionConfig().NotionKey
-	notionDB := config.GetNotionConfig().NotionDB
-	client := notion.NewClient(notionKey)
-	notionClient := no.NewNotionService(client, notionDB)
-	srv := google.GetCalendarFromGoogle()
-
 	// Google
-	// cal id
+	// Google Calendar Service
+	srv := google.GetCalendarFromGoogle()
+	// G-Cal ID; Primary refers to the authorized user
 	calendarID := "primary"
-	// create calendar client
+	// Create G-Cal Client
 	googleCalClient := google.NewGCalService(srv)
 
-	// get auth user google calendar
+	// Notion
+	// Getting Notion Secret Key and Database ID from .env
+	notionKey := config.GetNotionConfig().NotionKey
+	notionDB := config.GetNotionConfig().NotionDB
+	// Create Notion Client with Specificed Database ID
+	client := notion.NewClient(notionKey)
+	notionClient := no.NewNotionService(client, notionDB)
+
+	// Get Authorized User for G-Cal
 	calList, err := googleCalClient.GetCalendars()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// filter and sort google calendar events
+	// Filter and Sort G-Cal Events
 	calToday, err := googleCalClient.FilterEvents(calList)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// get notion events
+	// Get Notion Events
 	notionToday, err := notionClient.GetEvents()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Check existing events
+	// Check Existing Events in Both Services
 	checkNeedsUpdate, notInNotion, err := notionClient.CheckExistingEvents(notionToday, calToday)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Instantiate Two Lists, Each for Updating Events in Respective Services
 	var notionNeedsUpdate []google.EventItem
 	var notionNeedsUpdateId []string
 
 	var gcalNeedsUpdate []notion.DatabaseQueryResponse
 	var gcalNeedsUpdateId []string
-	// need to get event from calToday with given gcal_id and compare to event in notion with same gcal_id
+	// Get matching event IDs
 	for _, check := range checkNeedsUpdate {
 		var idx int
 		for i, item := range calToday {
@@ -68,11 +69,14 @@ func main() {
 				idx = i
 			}
 		}
+
+		// Retrieve Last Edited Time for Given Event in G-Cal
 		currEventGCal := calToday[idx]
 		currEventGCalT, _ := time.Parse(time.RFC3339, currEventGCal.EditedDate)
 		d := 60 * time.Second
 		currEventGCalTime := currEventGCalT.Truncate(d)
 
+		// Retrieve Last Edited Time for Given Event in Notion
 		notionUpdate, _ := client.QueryDatabase(ctx, notionDB, &notion.DatabaseQuery{
 			Filter: &notion.DatabaseQueryFilter{
 				Property: "GCal_ID",
@@ -84,36 +88,42 @@ func main() {
 		currEventNotion := notionUpdate.Results[0]
 		currEventNotionTime, _ := time.Parse(time.RFC3339, currEventNotion.Properties.(notion.DatabasePageProperties)["Edited"].LastEditedTime.Format(time.RFC3339))
 
+		// Compare Last Edited Times and Append Event to list depending on which Service Needs to Update
 		compareCase1 := currEventGCalTime.Before(currEventNotionTime)
 		compareCase2 := currEventGCalTime.After(currEventNotionTime)
 		if compareCase1 {
+			// If Notion's Last Edited Time is more Recent, Update in G-Cal
 			gcalNeedsUpdate = append(gcalNeedsUpdate, notionUpdate)
 			gcalNeedsUpdateId = append(gcalNeedsUpdateId, calToday[idx].ID)
 		} else if compareCase2 {
+			// If G-Cal's Last Edited Time is more Recent, Update in Notion
 			notionNeedsUpdate = append(notionNeedsUpdate, calToday[idx])
 			notionNeedsUpdateId = append(notionNeedsUpdateId, notionUpdate.Results[0].ID)
 		} else {
+			// If Last Edited Time is same, no need to Update Given Event
 			continue
 		}
 	}
 
-	// Update Google Calendar
+	// Update Event(s) in G-Cal
 	err = googleCalClient.UpdateCalendar(calendarID, gcalNeedsUpdate, gcalNeedsUpdateId)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Update Notion Database
+	// Update Event(s) in Notion
 	err = notionClient.UpdateEvents(notionNeedsUpdate, notionNeedsUpdateId)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Create Event(s) in Notion
 	err = notionClient.Create(notInNotion)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Check for new events in Notion that are not in GCal
+
+	// Create Event(s) in G-Cal
 	notInGCal, err := notionClient.CheckNewEvents()
 	if err != nil {
 		log.Fatal(err)
